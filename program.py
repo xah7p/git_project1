0c38777fb396204a77e970dd1bff7f8b92e9d246
xah7p <126350857+xah7p@users.noreply.github.com>
2023-12-01 18:09:31 +0300

    Update

--- program.py
+++ program.py
@@ -1,8 +1,8 @@
 def main():
     print('My first git program')
-    print('yeee')
-    print('git')
-    print('jest')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
